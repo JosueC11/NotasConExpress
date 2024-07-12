1b8ee820cfd67a045ab0d005df1b27f2af3a5e7c
Josue <Josue@DESKTOP-NKF5GC8>
2024-07-11 19:45:53 -0600

    Creando carpera public para el html,css y agregando metodos http

--- Server.js
+++ Server.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const path = require('path');
+
+const PUBLIC = path.join(__dirname,'public');
+
 const PUERTO = 3000;
 const app = express();
+
+app.use(express.static(PUBLIC));
 
 app.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto: ${PUERTO}`);
 });
 
-app.get('/home', (req, res) => {
-    res.send('Hola Mundo');
+
+// API //
+
+// GET
+app.get('/notas', (req, res) => {
+    res.sendFile(path.join(PUBLIC,'home.html'));
 });
+
+app.get('/notas/id', (req, res) => {
+    res.sendFile(path.join(PUBLIC,'home.html'));
+});
+
+// POST
+app.post('/notas', (req, res) => {
+    res.sendFile(path.join(PUBLIC,'home.html'));
+});
+
+// PUT
+app.put('/notas/id', (req, res) => {
+    res.sendFile(path.join(PUBLIC,'home.html'));
+});
+
+// DELETE
+app.delete('/notas/id', (req, res) => {
+    res.sendFile(path.join(PUBLIC,'home.html'));
+});
+
+
+let nota = {
+    idNota: 0,
+    titulo: '',
+    contenido: '',
+    fechaCreacion: '',
+    fechaUltimaModificacion: '',
+    Etiquetas: {}
+}
